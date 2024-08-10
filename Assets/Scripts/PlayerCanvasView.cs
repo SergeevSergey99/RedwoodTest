648dc7df4fa501f58fc64fb9dacd6b47b90d4163
Sergey <Sergeev_sergey99@mail.ru>
2024-08-10 16:27:33 +0300

    ...

--- Assets/Scripts/PlayerCanvasView.cs
+++ Assets/Scripts/PlayerCanvasView.cs
@@ -6,21 +6,13 @@ using UnityEngine;
 
 public class PlayerCanvasView : MonoBehaviour
 {
-    [SerializeField] private TMP_Text _healthText;
     [SerializeField] private TMP_Text _bulletsText;
     
     void Start()
     {
-        PlayerData.OnHealthChanged += SetHealthText;
         PlayerData.OnBulletsChanged += SetBulletsText;
         
-        SetHealthText(PlayerData.Instance.Health);
         SetBulletsText(PlayerData.Instance.Bullets);
-    }
-    
-    void SetHealthText(int health)
-    {
-        _healthText.text = $"{health} / {PlayerData.Instance.MaxHealth}";
     }
     
     void SetBulletsText(int bullets)
@@ -30,7 +22,6 @@ public class PlayerCanvasView : MonoBehaviour
 
     private void OnDestroy()
     {
-        PlayerData.OnHealthChanged -= SetHealthText;
         PlayerData.OnBulletsChanged -= SetBulletsText;
     }
 }
